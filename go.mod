0514c27eff0ffc6b5de02e99dc1c284bfa1bd3cd
midoks <midoks@163.com>
2022-04-11 14:34:03 +0800

    up

--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/midoks/simdht
 
 go 1.17
 
-require github.com/urfave/cli v1.22.5
+require (
+	github.com/fasthttp/router v1.4.8
+	github.com/urfave/cli v1.22.5
+	github.com/valyala/fasthttp v1.35.0
+)
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/savsgio/gotils v0.0.0-20220401102855-e56b59f40436 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
--- internal/cmd/service.go
+++ internal/cmd/service.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/fasthttp/router"
 	"github.com/urfave/cli"
+	"github.com/valyala/fasthttp"
 )
 
 var Service = cli.Command{
@@ -21,5 +26,44 @@ func runAllService(c *cli.Context) error {
 	// }
 
 	// app.Start(conf.Web.HttpPort)
+
+	r := router.New()
+	r.GET("/", Index)
+	r.GET("/hello/{name}", Hello)
+
+	if err := fasthttp.ListenAndServe(":11010", r.Handler); err != nil {
+		log.Fatalf("Error in ListenAndServe: %v", err)
+	}
 	return nil
+}
+
+func Hello(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, %s!\n", ctx.UserValue("name"))
+}
+func Index(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, world!\n\n")
+
+	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+
+	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+
+	ctx.SetContentType("text/plain; charset=utf8")
+
+	// Set arbitrary headers
+	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+	// Set cookies
+	var c fasthttp.Cookie
+	c.SetKey("cookie-name")
+	c.SetValue("cookie-value")
+	ctx.Response.Header.SetCookie(&c)
 }
